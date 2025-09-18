2d43b9017fd6f801a9ff5da04050289b33faaf3f
Alireza Ghadimi <ghadimi@yorku.ca>
2025-09-18 16:44:05 -0400

    some unknown changes

--- my_ws/build/cpmr_ch2/prefix_override/sitecustomize.py
+++ my_ws/build/cpmr_ch2/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/common/Documents/argh/my_ws/install/cpmr_ch2'
+    sys.prefix = sys.exec_prefix = '/home/common/Documents/myws/my_ws/install/cpmr_ch2'
